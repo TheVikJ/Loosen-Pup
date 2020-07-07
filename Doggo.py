fe2a56b6385a15402158b45da136ef0f172d5982
Vikram <33194767+Vik0105@users.noreply.github.com>
2020-07-07 14:06:07 +0530

    Update Doggo.py

--- Doggo.py
+++ Doggo.py
@@ -3,7 +3,7 @@ import time
 import os
 
 while True:
-    dog.getDog(directory='/users/<USER>/lenovo/Desktop', filename='dog')
+    dog.getDog(directory='/users/<USER>/Desktop', filename='dog')
     print("How about a break? Here's a new doggo pic! V◕ฺω◕ฺV")
     time.sleep(2700)
     os.remove("/users/<USER>/Desktop/dog.jpg")
